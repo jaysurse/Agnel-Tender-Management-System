7c4ec1b952077abd782d8eecc4bf5d44521a6ea2
VivekJangam126 <vivekjangam73@gmail.com>
2026-01-17 11:58:18 +0530

    fetch

--- client/src/services/proposalService.js
+++ client/src/services/proposalService.js
@@ -4,11 +4,14 @@ export const proposalService = {
   listMine: (token) => apiRequest('/proposals/mine', { token }),
   createDraft: (tenderId, token) => apiRequest('/proposals', { method: 'POST', token, body: { tenderId } }),
   getProposal: (id, token) => apiRequest(`/proposals/${id}`, { token }),
-  saveSectionResponse: (proposalId, sectionId, content, token) =>
-    apiRequest(`/proposals/${proposalId}/sections/${sectionId}`, {
-      method: 'PUT',
-      token,
-      body: { content },
-    }),
-  submit: (proposalId, token) => apiRequest(`/proposals/${proposalId}/submit`, { method: 'POST', token }),
+    listMine: (token) => apiRequest('/api/proposals/mine', { token }),
+    createDraft: (tenderId, token) => apiRequest('/api/proposals', { method: 'POST', token, body: { tenderId } }),
+    getProposal: (id, token) => apiRequest(`/api/proposals/${id}`, { token }),
+    saveSectionResponse: (proposalId, sectionId, content, token) =>
+      apiRequest(`/api/proposals/${proposalId}/sections/${sectionId}`, {
+        method: 'PUT',
+        token,
+        body: { content },
+      }),
+    submit: (proposalId, token) => apiRequest(`/api/proposals/${proposalId}/submit`, { method: 'POST', token }),
 };
